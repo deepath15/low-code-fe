7d51e97597fb98738d67fd9255eab9ed699ceaff
deepath15 <deepath1506@gmail.com>
2025-05-18 10:00:08 +0530

    trying to resolve the generate code  endpoint

--- src/sketchCode/Editor.jsx
+++ src/sketchCode/Editor.jsx
@@ -36,7 +36,7 @@ const Editor = ({
 
     try {
       const response = await axios.post(
-        "https://low-code-server.deepath.tech:8000/generate-code",
+        "https://low-code-ai.deepath.tech/generate-code",
         {
           image: dataUrl,
         }
